33a3ba43dcfe85d09299abfb34f49815419066f7
harryisawesome123 <harish.rahukulan25@ycdsbk12.ca>
2024-03-07 13:28:26 -0500

    Assignment

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,65 @@
 import processing.core.PApplet;
+import java.util.Random;
+
+/**
+ * A program Sketch.java is a house on a beauitful day drawing that has a random location each time
+ * @Author: H.Rahukulan 
+ *
+ */
 
 public class Sketch extends PApplet {
+
+  // Declare Variables
+  float HouseY;
+  float HouseX;
+  String CurrentTime;
+  float HouseSize = 150;
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    // Size of my background
+    size(800, 800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+
+    // Colour of the sky
+    background(52, 151, 227);
+
+    // Randomizer
+    HouseX = random(100, width - 250);
+    HouseY = random(100, height - 200);
+
+    // Set Time
+    CurrentTime = hour() + ":" + minute() + ":" + second();
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // House
+    if (HouseY < width / 2 || HouseY == width / 2){
+      fill(82, 51, 8);
+    }
+      else if (HouseY > width / 2){
+      fill(168, 50, 164);
+    }
+    rect(HouseX, HouseY, HouseSize, HouseSize);
+
+    // ROOF
+    fill(13, 1, 23);
+    triangle(HouseX - 25, HouseY, HouseX + 75, HouseY - 100, HouseX + 175, HouseY);
+    
+    // Sun
+    fill(233, 237, 2);
+    ellipse(HouseX - 120, HouseY - 120, 75, 75);
+
+    // Door
+    fill(237, 61, 2);
+    rect(HouseX + 60, HouseY + 90, 30, 60);
+    
+    // Time
+    fill(255);
+    textSize(32);
+    text("Current Time: " + CurrentTime, 50, 50);
+
   }
-  
-  // define other methods down here.
 }
